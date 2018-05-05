46d4274b94aed7722c04ede62b88f8404aee60aa
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2018-05-05 23:46:52 +0530

    AP_IOMCU: add support for iofirmware update

--- libraries/AP_IOMCU/AP_IOMCU.cpp
+++ libraries/AP_IOMCU/AP_IOMCU.cpp
@@ -677,6 +677,8 @@ bool AP_IOMCU::check_crc(void)
         hal.console->printf("IOMCU: CRC ok\n");
         crc_is_ok = true;
         return true;
+    } else {
+        hal.console->printf("IOMCU: CRC mismatch expected: 0x%X got: 0x%X\n", crc, io_crc);
     }
 
     const uint16_t magic = REBOOT_BL_MAGIC;
